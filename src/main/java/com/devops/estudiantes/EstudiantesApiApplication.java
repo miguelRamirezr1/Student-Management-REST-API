87d15d1fd917ddfcaecb1270dda64cf5235df407
miguelRamirezr1 <miguel.ramirezr@upb.edu.co>
2026-02-11 03:18:27 +0100

    Spring Boot project initialized with Spring Web, Spring Boot DevTools, Lombok and Validation dependencies

---
+++ src/main/java/com/devops/estudiantes/EstudiantesApiApplication.java
@@ -0,0 +1,13 @@
+package com.devops.estudiantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudiantesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudiantesApiApplication.class, args);
+	}
+
+}
